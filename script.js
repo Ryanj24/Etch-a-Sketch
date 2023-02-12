24cb2d9b8fcbb28d9761bdd63f33bbc1d99b38a1
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-12 15:46:33 +0000

    Added functionality for a clear button and RGB colour selector

--- script.js
+++ script.js
@@ -6,19 +6,58 @@ const display = document.querySelector("#sizeOutput");
 // Select the grid container itself
 const canvas = document.querySelector(".grid");
 
+// Select the clear button from the DOM
+const clear = document.querySelector("#clearButton");
+
+// Select the colour selector from the DOM
+const colourPicker = document.querySelector("#rgbPicker");
+
+
 // Select the range slider element that carries the value of the size 
 let gridDimensions = document.querySelector("#gridSize");
 
-// Display default grid of 8 x 8
+// Create default grid of 8 x 8
 for (i = 0; i < parseInt(gridDimensions.value)*parseInt(gridDimensions.value); i++) {
+    
+    // Create div element
     const gridDiv = document.createElement("div");
+    
+    // Add class of "grid-item" to the div
     gridDiv.classList.add("grid-item");
+    
+    // Add event listener to the div so when mouse moved over it, background changes
     gridDiv.addEventListener("mouseover", function() {
-        gridDiv.setAttribute("style", "background-color: black;")
+        gridDiv.setAttribute("style", `background-color: ${colourPicker.value};`)
     })
+    
+    // Add the div to the canvas
     canvas.appendChild(gridDiv);
 }
 
+// Clear button that clears the canvas for the user
+clear.addEventListener("click", function() {
+    
+    // Remove divs currently in use
+    canvas.innerHTML = "";
+
+    // re-create default grid of 8 x 8
+    for (i = 0; i < parseInt(gridDimensions.value)*parseInt(gridDimensions.value); i++) {
+        
+        // Create div element
+        const gridDiv = document.createElement("div");
+        
+        // Add class of "grid-item" to the div
+        gridDiv.classList.add("grid-item");
+        
+        // Add event listener to the div so when mouse moved over it, background changes
+        gridDiv.addEventListener("mouseover", function() {
+            gridDiv.setAttribute("style", `background-color: ${colourPicker.value};`)
+        })
+        
+        // Add the div to the canvas
+        canvas.appendChild(gridDiv);
+    }
+})
 
 // Add an event listener to the range slider input
 gridDimensions.addEventListener("input", function() {
@@ -39,11 +78,19 @@ gridDimensions.addEventListener("input", function() {
 
     // Loop to create the divs that form the grid using the dimensions specified
     for (i = 0; i < size*size; i++) {
+        
+        // Create div element
         const gridDiv = document.createElement("div");
+        
+        // Add the "grid-item" class
         gridDiv.classList.add("grid-item");
+        
+        // Add mouseover event listener to change background colour
         gridDiv.addEventListener("mouseover", function() {
-            gridDiv.setAttribute("style", "background-color: black;")
+            gridDiv.setAttribute("style", `background-color: ${colourPicker.value};`)
         })
+        
+        // Add the div to the canvas to create grid
         canvas.appendChild(gridDiv);
     }
 })
